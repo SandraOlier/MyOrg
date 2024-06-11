fc7a7a308cf5a93efe2257ef87c47af7a9ebd6e9
Sandra Olier <sahndraelizabetholier@gmail.com>
2024-06-11 18:27:53 -0300

    subiendo proyecto

---
+++ src/componentes/MiOrg/index.js
@@ -0,0 +1,20 @@
+import {useState} from "react"
+import "./MiOrg.css"
+
+const MiOrg = (props) => {
+    //Estado - hooks
+    //useState 
+//const [nombreVariable,funcionActualiza] = useState(valorInicial)
+console.log(props)  
+//const [mostrar, actualizarMostrar] = useState(true)
+//const manejarClick = () => {
+  //  console.log("Mostrar/Ocultar elemento", !mostrar)
+ //   actualizarMostrar(!mostrar)
+//}
+
+    return <section className="orgSection">
+<h3 className="title">Mi organización</h3>
+<img src="/img/Botão add-01 1.png" alt="add" onClick={props.cambiarMostrar}/>
+    </section>
+}
+export default MiOrg
